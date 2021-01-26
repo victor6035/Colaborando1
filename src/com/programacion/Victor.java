55463b3a6bee9d3984382c0ffa1d59c973ae3e2a
VictorSaaPerez <vsaaperez@danielcastelao.org>
2021-01-26 11:26:19 +0100

    modifico la segunda rama

--- src/com/programacion/Victor.java
+++ src/com/programacion/Victor.java
@@ -5,4 +5,7 @@ public class Victor {
     public void amosar(){
         System.out.println("soy victor");
     }
+    public void colaborando(){
+        System.out.println("esta es la segunda rama");
+    }
 }
