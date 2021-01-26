260a725e668126cc7541185e90e7411f2de38951
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-01-26 11:14:18 +0100

    2 rama creada

--- src/com/programacion/Gabriel.java
+++ src/com/programacion/Gabriel.java
@@ -4,5 +4,6 @@ public class Gabriel {
 
     public void colaboorando(){
         System.out.println("Esta es la rama del colaborador gabriel");
+        System.out.println("Esta es la segunda rama del colaborador gabriel");
     }
 }
