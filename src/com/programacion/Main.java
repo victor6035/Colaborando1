fb18c08701c86a4ee270acb62a6c53194708d19e
VictorSaaPerez <vsaaperez@danielcastelao.org>
2021-02-02 10:52:53 +0100

    he arreglado el fallo en la entra close #1

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -3,6 +3,6 @@ package com.programacion;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //he arreglado el error de entrada
     }
 }
--- src/com/programacion/Victor.java
+++ src/com/programacion/Victor.java
@@ -2,6 +2,8 @@ package com.programacion;
 
 public class Victor {
 
+
+
     public void amosar(){
         System.out.println("soy victor");
     }
