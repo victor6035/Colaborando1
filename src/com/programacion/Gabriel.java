0efc5b9ea86cf95c7d6cc7b4bf62b35eb6375627
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-02-02 11:24:15 +0100

    finalice faloo close #3

--- src/com/programacion/Gabriel.java
+++ src/com/programacion/Gabriel.java
@@ -5,5 +5,7 @@ public class Gabriel {
     public void colaboorando(){
         System.out.println("Esta es la rama del colaborador gabriel");
         System.out.println("Esta es la segunda rama del colaborador gabriel");
+
+        //finalice fallo
     }
 }
